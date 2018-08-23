9061a779e9c55c1e67ca829ef82147d41526052f
Olivia Macedo <liom62313@gmail.com>
2018-08-23 02:21:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, board_pos)
-
+  temp = board[board_pos]
+  if(temp == " " || temp == "")
+    return false
+  else
+    return true
+  end
 end
